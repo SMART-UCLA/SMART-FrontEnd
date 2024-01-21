459d2c5668aa53f332145fed0b7249aabc7371fc
GeneKung <52974083+GeneKung@users.noreply.github.com>
2024-01-20 19:18:33 -0800

    Clean up auth config code

--- smart_frontend/src/auth/authConfiguration.js
+++ smart_frontend/src/auth/authConfiguration.js
@@ -1,32 +1,10 @@
-/*
-const { auth } = require('express-openid-connect');
-
-export const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: '86a8359a39e968f21cdd43b8897967f013f06bc21ed29904ee1b9885a9939622',
-  baseURL: 'http://localhost:3000/SMART-FrontEnd',
-  clientID: 'OVs6wasqgkibgaaY58XxY5bSlktODzhQ',
-  issuerBaseURL: 'https://dev-xdx10w8664v1a2yz.us.auth0.com',
-  domain: 'dev-xdx10w8664v1a2yz.us.auth0.com',
-  clientID: 'OVs6wasqgkibgaaY58XxY5bSlktODzhQ',
-};
-
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
-
-// req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
-*/
-
 import { Auth0Provider } from '@auth0/auth0-react';
 
 const AuthProvider = ({ children }) => {
     const domain = 'dev-xdx10w8664v1a2yz.us.auth0.com';
     const clientId = 'OVs6wasqgkibgaaY58XxY5bSlktODzhQ';
     const redirectUri = 'http://localhost:3000/SMART-FrontEnd'
+
     return (
       <Auth0Provider
         domain={domain}
--- smart_frontend/src/components/Home.js
+++ smart_frontend/src/components/Home.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React  from 'react';
 import smartImg from './smart.png';
 import Box from '@mui/material/Box';
 import { useAuth0 } from '@auth0/auth0-react';
 
 function Home() {
-  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+  const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0();
 
   return (
     <div style={{ textAlign: 'center' }}>
@@ -27,6 +27,9 @@ function Home() {
           >
             This site is a web display of all lunar magnetic field data collected from the Apollo 12, 15, and Apollo 16 missions. It is open to the general public, and all are welcome to use it for whatever purposes at no cost.
           </Box>
+          <button onClick={() => logout()}>
+            log out
+          </button>
         </div>
       ) : (
         <p>
