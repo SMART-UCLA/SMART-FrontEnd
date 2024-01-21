3e23887d7c8532bc4b8b1dad12d15adf48140bf9
GeneKung <52974083+GeneKung@users.noreply.github.com>
2024-01-20 17:45:22 -0800

    Corrected allowed URL callback path

--- smart_frontend/src/App.js
+++ smart_frontend/src/App.js
@@ -50,7 +50,7 @@ function App() {
               />}
             /> */}
         </Routes>
-        </AuthProvider>
+      </AuthProvider>
     </HashRouter>
   );
 }
--- smart_frontend/src/auth/authConfiguration.js
+++ smart_frontend/src/auth/authConfiguration.js
@@ -26,12 +26,12 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const AuthProvider = ({ children }) => {
     const domain = 'dev-xdx10w8664v1a2yz.us.auth0.com';
     const clientId = 'OVs6wasqgkibgaaY58XxY5bSlktODzhQ';
-  
+    const redirectUri = 'http://localhost:3000/SMART-FrontEnd'
     return (
       <Auth0Provider
         domain={domain}
         clientId={clientId}
-        redirectUri={window.location.origin}
+        authorizationParams={{ redirect_uri: redirectUri }}
       >
         {children}
       </Auth0Provider>
