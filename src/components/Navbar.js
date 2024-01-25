55697ce582f8d358a375a9271961028054eb3d27
GeneKung <52974083+GeneKung@users.noreply.github.com>
2024-01-25 13:34:18 -0800

    Moved navbar to only be visible when user is logged in

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,12 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import smartLogo from './logo.png';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const Navbar = () => {
   const navigate = useNavigate();
+
+  const { logout } = useAuth0();
 
   const handleNavLinkClick = (path) => {
     navigate(path);
@@ -29,6 +32,9 @@ const Navbar = () => {
           </a>
           <a href="#" className="text-black" onClick={() => handleNavLinkClick('/contact')}>
             Contact
+          </a>
+          <a href="#" className="text-black" onClick={() => logout()}>
+            Logout
           </a>
         </div>
       </div>
