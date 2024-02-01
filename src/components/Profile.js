570637cb0b1abca172340b72c41324793f46b62c
GeneKung <52974083+GeneKung@users.noreply.github.com>
2024-02-01 00:20:38 -0800

    Update users functionality works

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,30 +7,31 @@ const Profile = () => {
   const domain = "dev-xdx10w8664v1a2yz.us.auth0.com";
   const { name, picture } = user;
 
+  const getUserMetadata = async () => {
+    try {
+      const accessToken = await getAccessTokenSilently({
+        authorizationParams: {
+          audience: `https://${domain}/api/v2/`,
+          scope: "read:current_user",
+        },
+      });
+
+      const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
+      const metadataResponse = await fetch(userDetailsByIdUrl, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      const responseData = await metadataResponse.json();
+      const userMetadata = responseData.user_metadata;
+      setUserMetadata(userMetadata);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
   useEffect(() => {
-    const getUserMetadata = async () => {
-      try {
-        const accessToken = await getAccessTokenSilently({
-          authorizationParams: {
-            audience: `https://${domain}/api/v2/`,
-            scope: "read:current_user",
-          },
-        });
-
-        const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
-        const metadataResponse = await fetch(userDetailsByIdUrl, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
-
-        const responseData = await metadataResponse.json();
-        const userMetadata = responseData.user_metadata; 
-        setUserMetadata(userMetadata);  
-      } catch (e) {
-        console.log(e.message);
-      }
-    };
     getUserMetadata();
   }, [getAccessTokenSilently, user?.sub]);
 
@@ -58,6 +59,7 @@ const Profile = () => {
 
       if (metadataResponse.ok) {
         console.log("Station number updated successfully.");
+        getUserMetadata();
       } else {
         console.log("Error updating station number");
       }
@@ -83,10 +85,13 @@ const Profile = () => {
           <div className="row">
               <div className="col-md-6 offset-md-3">
               {userMetadata ? (
-                  <pre>{userMetadata[user.sub].station_number}</pre>
+                  <pre>{userMetadata.station_number}</pre>
                 ) : (
                   "No user metadata defined"
                 )}
+              </div>
+              <div>
+                <button onClick={() => updateStationNumber("this3")}>Save</button>
               </div>
           </div>
       </div>
@@ -95,73 +100,3 @@ const Profile = () => {
 
 export default Profile;
 
-
-/*
-  useEffect(() => {
-    const getUserMetadata = async () => {
-      const domain = "dev-xdx10w8664v1a2yz.us.auth0.com";
-
-      try {
-        const accessToken = await getAccessTokenSilently({
-          audience: `https://${domain}/api/v2/`,
-          scope: "read:current_user",
-          ignoreCache: true,
-        });
-
-        const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
-
-        const metadataResponse = await fetch(userDetailsByIdUrl, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
-
-        const { user_metadata } = await metadataResponse.json();
-        console.log("URL:", userDetailsByIdUrl);
-        console.log("Headers:", {
-            Authorization: `Bearer ${accessToken}`,
-        });
-        console.log("Response:", metadataResponse);
-        console.log("here1");
-        console.log(user_metadata);
-
-        setUserMetadata(user_metadata);
-      } catch (e) {
-        console.log(e.message);
-      }
-    };
-
-    getUserMetadata();
-  }, [getAccessTokenSilently, user]);
-
-  const updateUserMetadata = async () => {
-    const domain = "dev-xdx10w8664v1a2yz.us.auth0.com";
-
-    try {
-      const accessToken = await getAccessTokenSilently({
-        audience: `https://${domain}/api/v2/`,
-        scope: "update:current_user_metadata",
-        ignoreCache: true,
-      });
-
-      const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
-
-      const { user_metadata } = await (
-        await fetch(userDetailsByIdUrl, {
-          method: "PATCH",
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            user_metadata: { testData: "testData" }, // Modify this object to update metadata as needed
-          }),
-        })
-      ).json();
-
-      setUserMetadata(user_metadata);
-    } catch (e) {
-      console.log(e.message);
-    }
-  };
-*/
