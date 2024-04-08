26c662dfb7b80e402d569b99aad658e8fa357d30
GeneKung <52974083+GeneKung@users.noreply.github.com>
2024-04-07 21:06:56 -0700

    Added and labeled the 6 maps

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React from 'react';
 import smartImg from './smart.png';
 import Box from '@mui/material/Box';
 import { useAuth0 } from '@auth0/auth0-react';
@@ -20,24 +20,52 @@ function Home() {
     <div style={{ textAlign: 'center' }}>
       <h1 className="text-4xl font-bold text-center mb-8">Welcome to SMART</h1>
       {isAuthenticated ? (
-        <div style={{ width: '50%', margin: 'auto' }}>
-          <Box
-            sx={{
-              textAlign: 'left',
-              fontSize: '1.15rem',
-              fontWeight: 'medium',
-              paddingLeft: 1.75,
-              paddingRight: 1.75,
-              paddingTop: 1.75,
-              paddingBottom: 1.75,
-              border: 1,
-              marginTop: '2rem',
-              marginBottom: '2rem',
-            }}
-          >
-            This site is a web display of all lunar magnetic field data collected from the Apollo 12, 15, and Apollo 16 missions. It is open to the general public, and all are welcome to use it for whatever purposes at no cost.
-          </Box>
-          <MapChart valueID={1}/>
+        <div>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <div style={{ width: '30%', margin: '10px', border: '1px solid #ccc', borderRadius: '8px' }}>
+              <Box
+                sx={{
+                  textAlign: 'left',
+                  fontSize: '1.15rem',
+                  fontWeight: 'medium',
+                  paddingLeft: 1.75,
+                  paddingRight: 1.75,
+                  paddingTop: 1.75,
+                  paddingBottom: 1.75,
+                  marginTop: '2rem',
+                  marginBottom: '2rem',
+                }}
+              >
+                This site is a web display of all lunar magnetic field data collected from the Apollo 12, 15, and Apollo 16 missions. It is open to the general public, and all are welcome to use it for whatever purposes at no cost.
+              </Box>
+            </div>
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
+            <div style={{ width: '30%', margin: '10px', border: '1px solid #ccc', borderRadius: '8px' }}>
+              <div style={{ fontWeight: 'bold', textDecoration: 'underline', marginTop: '30px' }}>X: First Derivative</div>
+              <MapChart valueID={1}/>
+            </div>
+            <div style={{ width: '30%', margin: '10px', border: '1px solid #ccc', borderRadius: '8px' }}>
+              <div style={{ fontWeight: 'bold', textDecoration: 'underline', marginTop: '30px' }}>Y: First Derivative</div>
+              <MapChart valueID={2}/>
+            </div>
+            <div style={{ width: '30%', margin: '10px', border: '1px solid #ccc', borderRadius: '8px' }}>
+              <div style={{ fontWeight: 'bold', textDecoration: 'underline', marginTop: '30px' }}>Z: First Derivative</div>
+              <MapChart valueID={3}/>
+            </div>
+            <div style={{ width: '30%', margin: '10px', border: '1px solid #ccc', borderRadius: '8px' }}>
+              <div style={{ fontWeight: 'bold', textDecoration: 'underline', marginTop: '30px' }}>X: Second Derivative</div>
+              <MapChart valueID={4}/>
+            </div>
+            <div style={{ width: '30%', margin: '10px', border: '1px solid #ccc', borderRadius: '8px' }}>
+              <div style={{ fontWeight: 'bold', textDecoration: 'underline', marginTop: '30px' }}>Y: Second Derivative</div>
+              <MapChart valueID={5}/>
+            </div>
+            <div style={{ width: '30%', margin: '10px', border: '1px solid #ccc', borderRadius: '8px' }}>
+              <div style={{ fontWeight: 'bold', textDecoration: 'underline', marginTop: '30px' }}>Z: Second Derivative</div>
+              <MapChart valueID={6}/>
+            </div>
+          </div>
         </div>
       ) : (
         <p>
@@ -51,9 +79,7 @@ function Home() {
           marginBottom: '20px',
         }}
       >
-
         <img src={smartImg} width="75%" height="60%" alt="SMART Image" className="mx-auto" />
-
       </span>
     </div>
   );
